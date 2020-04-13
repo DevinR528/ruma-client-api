e25b72c752e432aa4b00899c789b9c552423b4c5
Devin R <devin.ragotzy@gmail.com>
2020-04-13 19:58:02 -0400

    remove docs, fix mod level docs, move tests to get_msg_events

--- src/r0/message/get_message_events.rs
+++ src/r0/message/get_message_events.rs
@@ -76,3 +76,80 @@ pub enum Direction {
     #[serde(rename = "f")]
     Forward,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{Direction, Request};
+
+    use std::convert::{TryFrom, TryInto};
+
+    use ruma_identifiers::RoomId;
+
+    use crate::r0::filter::{LazyLoadOptions, RoomEventFilter};
+
+    #[test]
+    fn test_serialize_some_room_event_filter() {
+        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
+        let filter = RoomEventFilter {
+            lazy_load_options: LazyLoadOptions::Enabled {
+                include_redundant_members: true,
+            },
+            rooms: Some(vec![room_id.clone()]),
+            not_rooms: vec!["room".into(), "room2".into(), "room3".into()],
+            not_types: vec!["type".into()],
+            ..Default::default()
+        };
+        let req = Request {
+            room_id,
+            from: "token".into(),
+            to: Some("token2".into()),
+            dir: Direction::Backward,
+            limit: Some(js_int::UInt::min_value()),
+            filter: Some(filter),
+        };
+
+        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
+        assert_eq!(
+            "from=token&to=token2&dir=b&limit=0&filter=%7B%22not_types%22%3A%5B%22type%22%5D%2C%22not_rooms%22%3A%5B%22room%22%2C%22room2%22%2C%22room3%22%5D%2C%22rooms%22%3A%5B%22%21roomid%3Aexample.org%22%5D%2C%22lazy_load_members%22%3Atrue%2C%22include_redundant_members%22%3Atrue%7D",
+            request.uri().query().unwrap()
+        );
+    }
+
+    #[test]
+    fn test_serialize_none_room_event_filter() {
+        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
+        let req = Request {
+            room_id,
+            from: "token".into(),
+            to: Some("token2".into()),
+            dir: Direction::Backward,
+            limit: Some(js_int::UInt::min_value()),
+            filter: None,
+        };
+
+        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
+        assert_eq!(
+            "from=token&to=token2&dir=b&limit=0",
+            request.uri().query().unwrap(),
+        );
+    }
+
+    #[test]
+    fn test_serialize_default_room_event_filter() {
+        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
+        let req = Request {
+            room_id,
+            from: "token".into(),
+            to: Some("token2".into()),
+            dir: Direction::Backward,
+            limit: Some(js_int::UInt::min_value()),
+            filter: Some(RoomEventFilter::default()),
+        };
+
+        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
+        assert_eq!(
+            "from=token&to=token2&dir=b&limit=0&filter=%7B%7D",
+            request.uri().query().unwrap(),
+        );
+    }
+}
--- src/serde/json_string.rs
+++ src/serde/json_string.rs
@@ -1,12 +1,10 @@
-//! De-/serialization functions for `Option<RoomEventFilter>` objects.
-//! Delegates to `js_int::UInt` to ensure integer size is within bounds.
+//! De-/serialization functions to and from json strings, allows the type to be used as a query string.
 
 use serde::{
     de::{Deserialize, DeserializeOwned, Deserializer, Error as _},
     ser::{Error as _, Serialize, Serializer},
 };
 
-/// Serialize a filter into a json string.
 pub fn serialize<T, S>(filter: T, serializer: S) -> Result<S::Ok, S::Error>
 where
     T: Serialize,
@@ -16,7 +14,6 @@ where
     serializer.serialize_str(&json)
 }
 
-/// Deserializes a filter from a json string.
 pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
 where
     T: DeserializeOwned,
@@ -24,71 +21,4 @@ where
 {
     let s = String::deserialize(deserializer)?;
     serde_json::from_str(&s).map_err(D::Error::custom)
-}
-
-#[cfg(test)]
-mod tests {
-    use std::convert::{TryFrom, TryInto};
-
-    use ruma_identifiers::RoomId;
-
-    use crate::r0::filter::{LazyLoadOptions, RoomEventFilter};
-    use crate::r0::message::get_message_events::{Direction, Request};
-
-    #[test]
-    fn test_serialize_some_room_event_filter() {
-        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
-        let filter = RoomEventFilter {
-            lazy_load_options: LazyLoadOptions::Enabled {
-                include_redundant_members: true,
-            },
-            rooms: Some(vec![room_id.clone()]),
-            not_rooms: vec!["room".into(), "room2".into(), "room3".into()],
-            not_types: vec!["type".into()],
-            ..Default::default()
-        };
-        let req = Request {
-            room_id,
-            from: "token".into(),
-            to: Some("token2".into()),
-            dir: Direction::Backward,
-            limit: Some(js_int::UInt::min_value()),
-            filter: Some(filter),
-        };
-
-        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
-        println!("{:?}", request.uri().query());
-    }
-
-    #[test]
-    fn test_serialize_none_room_event_filter() {
-        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
-        let req = Request {
-            room_id,
-            from: "token".into(),
-            to: Some("token2".into()),
-            dir: Direction::Backward,
-            limit: Some(js_int::UInt::min_value()),
-            filter: None,
-        };
-
-        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
-        println!("{:?}", request.uri());
-    }
-
-    #[test]
-    fn test_serialize_default_room_event_filter() {
-        let room_id = RoomId::try_from("!roomid:example.org").unwrap();
-        let req = Request {
-            room_id,
-            from: "token".into(),
-            to: Some("token2".into()),
-            dir: Direction::Backward,
-            limit: Some(js_int::UInt::min_value()),
-            filter: Some(RoomEventFilter::default()),
-        };
-
-        let request: http::Request<Vec<u8>> = req.try_into().unwrap();
-        println!("{:?}", request.uri());
-    }
 }
